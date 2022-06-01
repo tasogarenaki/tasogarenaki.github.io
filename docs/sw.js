ab6fa6f3086f82ecd74eca60914aace1a0d2a5ab
Terry <terry.chan2h@gmail.com>
2022-06-01 23:21:21 +0200

    v1

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220601.2119';
+const cacheName = 'chirpy-20220601.2121';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
