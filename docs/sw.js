78e1f86832c7dcf240c2141865ba1e53eb3fb458
tasogarenaki <terry.chan2h@gmail.com>
2022-09-01 19:30:26 +0200

    change the Titel

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220901.1728';
+const cacheName = 'chirpy-20220901.1730';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
