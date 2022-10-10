a0b55250cb6726509a3c895e328a11ee207c53b4
tasogarenaki <terry.chan2h@gmail.com>
2022-10-10 21:56:18 +0200

    SLList

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220911.1555';
+const cacheName = 'chirpy-20221010.1955';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
