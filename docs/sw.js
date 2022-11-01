5ffe19e023bc8e4bc6950b396e53185f8d15e64f
tasogarenaki <terry.chan2h@gmail.com>
2022-11-01 20:58:20 +0100

    override

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221026.1852';
+const cacheName = 'chirpy-20221101.1957';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
