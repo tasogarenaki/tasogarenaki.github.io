e41485d4cae2307d08d824ce3ffc04c6f1b26549
tasogarenaki <terry.chan2h@gmail.com>
2022-11-02 20:22:39 +0100

    inheritance

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221101.1957';
+const cacheName = 'chirpy-20221102.1922';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
