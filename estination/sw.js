7d9d0d6c7235fbf6fe9747df6fc8122d56b8fe0e
tasogarenaki <terry.chan2h@gmail.com>
2022-11-21 20:29:34 +0100

    Array Deque

--- estination/sw.js
+++ estination/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221016.1546';
+const cacheName = 'chirpy-20221121.1929';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
