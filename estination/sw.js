7daeace197c345c4320d0cf99449ee6e89b0e044
tasogarenaki <terry.chan2h@gmail.com>
2022-11-29 20:55:50 +0100

    4.2

--- estination/sw.js
+++ estination/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221121.1929';
+const cacheName = 'chirpy-20221129.1955';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
