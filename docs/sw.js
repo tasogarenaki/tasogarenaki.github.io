afbe0ca98f9710502870ceec19a586c52892f78c
tasogarenaki <terry.chan2h@gmail.com>
2022-12-09 16:04:54 +0100

    finished lecture 9

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221123.1956';
+const cacheName = 'chirpy-20221209.1504';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
