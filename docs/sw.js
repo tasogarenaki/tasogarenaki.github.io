c00369e9dea66490678ef3fe6174497e994913e2
tasogarenaki <terry.chan2h@gmail.com>
2023-01-14 17:08:12 +0100

    Polymorphism

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221209.1504';
+const cacheName = 'chirpy-20230114.1607';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
