4fc85577328aabe3414f168252ae9d54d3629c21
tasogarenaki <terry.chan2h@gmail.com>
2023-06-23 14:00:16 +0200

    Chapter 5

--- docs/estination/sw.js
+++ docs/estination/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221129.1955';
+const cacheName = 'chirpy-20230127.1551';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230116.1534';
+const cacheName = 'chirpy-20230623.1159';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
