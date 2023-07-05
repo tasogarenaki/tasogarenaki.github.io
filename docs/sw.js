55aeddc2ad8a297439c69ea17cbe57252507a1b2
tasogarenaki <terry.chan2h@gmail.com>
2023-07-05 11:50:34 +0200

    finished chapter 7

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230626.1310';
+const cacheName = 'chirpy-20230705.0950';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
