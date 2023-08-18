6bd094f96159279187139f1c60c916b31b4f12d3
tasogarenaki <terry.chan2h@gmail.com>
2023-08-18 12:02:02 +0200

    Asymptotics

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230705.0950';
+const cacheName = 'chirpy-20230818.1001';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
