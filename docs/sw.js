eabc52d29cf2242914d782b0cb695b8bba83c3c3
tasogarenaki <terry.chan2h@gmail.com>
2023-08-24 14:51:45 +0200

    ADT

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230821.1445';
+const cacheName = 'chirpy-20230824.1251';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
