4d1261abdfd20989b75a117a9778123093ca3c48
tasogarenaki <terry.chan2h@gmail.com>
2023-08-29 18:00:34 +0200

    Hashing

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230828.1449';
+const cacheName = 'chirpy-20230829.1600';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
