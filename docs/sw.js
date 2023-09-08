2210c130827b24b27322c082ef0438286080674c
tasogarenaki <terry.chan2h@gmail.com>
2023-09-08 16:41:46 +0200

    graph traversals

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230907.1524';
+const cacheName = 'chirpy-20230908.1441';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
