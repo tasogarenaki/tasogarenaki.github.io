5e9e0dc63c464a376d4692f5799233457da8fe97
tasogarenaki <terry.chan2h@gmail.com>
2023-09-13 15:37:26 +0200

    shortest paths

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230908.1441';
+const cacheName = 'chirpy-20230913.1337';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
