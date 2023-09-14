a8972f03e27d2f61d3af69dd4be814f09bb089d6
tasogarenaki <terry.chan2h@gmail.com>
2023-09-14 11:22:20 +0200

    spanning tree

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230913.1337';
+const cacheName = 'chirpy-20230914.0922';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
