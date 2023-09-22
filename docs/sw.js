9e43e8da8cd414e760d1c66f4c089e58b7104bed
tasogarenaki <terry.chan2h@gmail.com>
2023-09-22 15:06:25 +0200

    radix sort

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230921.1238';
+const cacheName = 'chirpy-20230922.1306';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
