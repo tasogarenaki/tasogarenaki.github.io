efc2b6e9435b96b3fd119765e6920803e6004c04
tasogarenaki <terry.chan2h@gmail.com>
2023-09-24 16:35:55 +0200

    Tries

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230922.1306';
+const cacheName = 'chirpy-20230924.1435';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
